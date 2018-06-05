a40e2dfeef971b92c95d229e58920c51a0668b1e
lcc2207 <lynn@fidmail.com>
2018-06-05 09:53:54 -0500

    metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
 name 'docker-demo'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Lynn Caldwell'
+maintainer_email 'lynn@scalr.com'
 license 'All Rights Reserved'
 description 'Installs/Configures docker-demo'
 long_description 'Installs/Configures docker-demo'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# issues_url 'https://github.com/<insert_org_here>/docker-demo/issues'
-# source_url 'https://github.com/<insert_org_here>/docker-demo'
+issues_url 'https://github.com/lcc2207/docker-demo-chef/issues'
+source_url 'https://github.com/lcc2207/docker-demo-chef'
 
 depends 'docker', '~> 4.2.0'
 depends 'chef-sugar'
